c00ae1b8e358e817a9a484d9ca4336261323fd71
sergij14 <sergi.jajanidze@gmail.com>
2021-01-26 21:29:48 +0400

    comm

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -12719,7 +12719,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50906" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51278" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
